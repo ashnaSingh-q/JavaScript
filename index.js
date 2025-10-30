e87008132f1dbcaee92fa824cdbcbdca52517516
ashna singh <khushithakur906@gmail.com>
2025-10-30 09:35:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let ar = "hello";
+console.log(typeof ar );
+console.log(2+3);
+let age = 5
+if(age<=15){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
+//loops
+let  a = [1,2,3,4,5];
+for(let value of a ){// if variable already is declares then no  need to put brackets
+    console.log(value);
+}
+//object with for in 
+//object ki key
+let obj = (name:"hek"class:"23");
+for(let key in obj){
+    console.log(key + ":" + obj[key]);
+
+console.log('$(key: $(obj[key])');
+} 
